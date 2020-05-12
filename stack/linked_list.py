3c0f16c7c6b62f161a878fcf452bb306e4275572
Jud Taylor <jud.taylor@protonmail.com>
2020-05-11 22:43:54 -0400

    repaired list_stack and linked list.

--- stack/linked_list.py
+++ stack/linked_list.py
@@ -13,30 +13,40 @@ class Node:
         self.next_node = new_next
 
 class LinkedList:
+    """A singly linked list of zero or more Nodes."""
     def __init__(self):
+        """Initially has an empty head (ditzy)."""
         # anchor node
         self.head = None
-
+        
+    def __str__(self):
+        return (f'<Linked List>')
+        
     def add(self, value):
+        """An element is added to the list by constructing
+        a new node containing the received value."""
         # value is a number not yet wrapped in a node
         new_node = Node(value)
-        hed = self.head
 
         # case - list is empty
-        if not hed:
-            hed = new_node
-
-        else: # head exists
-            old_head_next = hed.next_node
-            hed = new_node
-            hed.next_node = old_head_next
+        if not self.head:
+            self.head = new_node
+        
+        else: # A head node exists.
+#             print(f'self.head: {self.head} - next node: {self.head.next_node}') # store
+            next = self.head
+            self.head = new_node
+#             print(f'self.head: {self.head} - next node: {self.head.next_node}')
+            self.head.set_next(next) # use stored
+#             print(f'self.head: {self.head} - next node: {self.head.next_node}')
             
-
     def remove(self):
         if not self.head:
             print("Nothing to pop.")
+            return None
             
         else:
+#             print(f'head: {self.head} - value: {self.head.value}')
             val = self.head.get_value()
             self.head = self.head.get_next()
             return val
