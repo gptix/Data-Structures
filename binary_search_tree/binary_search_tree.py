34c3d0a78d495dc8b20d7661e30f6acc9d4be5f3
Jud Taylor <jud.taylor@protonmail.com>
2020-05-15 10:04:04 -0400

    Corrected need for 'node' param.  Functions only require 'self'

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 """
 Binary search trees are a data structure that enforce an ordering over 
 the data they store. That ordering in turn makes it a lot more efficient 
@@ -17,53 +19,18 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, target):
-#         print(f'Target: {target} -- current-node-value: {self.value}')
-        if self.value == target:
-#             print( "Already in the tree.")
-            return
-        
-        # should I look left, or right?
-        elif target < self.value:
-#             print("I should look left.")
-#             # I should look left.
-#             # is there already something there?
+        if target < self.value: # should I look left, or right?
             if not self.left:
-#                 print("Nothing to the left, so insert.")
-                self.left = BSTNode(target)
-#                 print(f"Added leaf {target}")
-            else:
-                if target > self.left.value:
-#                     print("I should insert, and not recurse.")
-                    new_node = BSTNode(target)
-                    new_node.left = self.left
-                    self.left = new_node
-#                     print(f"Inserted non-leaf {target}")
-                    
-#                 # I should recurse
-                else:  
-#                     print("I will recurse to the left.")
-                    self.left.insert(target)
-        else:
-#             print("I should look right.")
+                self.left = BSTNode(target)  # add node as left child
+            else: 
+                self.left.insert(target) # recurse left
+        else: # target is >= self.value, look right
             if not self.right:
-#                 print("I should add a new leaf to the right, and not recurse.") 
-                new_node = BSTNode(target)
-                new_node.right = self.right
-                self.right = new_node
-#                 print(f"Inserted non-leaf {target}")
+                self.right = BSTNode(target) # add node as right child
             else:
-                if target > self.right.value:
-#                     print("I should insert, and not recurse.")
-                    new_node = BSTNode(target)
-                    new_node.right = self.right
-                    self.right = new_node
-#                     print(f"Inserted non-leaf {target}")
-                else:
-#                     print("I will recurse to the right.")
-                    self.right.insert(target)
-                    
-                
-    # Return True if the tree contains the value
+                self.right.insert(target) # recurse right
+
+                    # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
         if self.value == target:
@@ -77,9 +44,9 @@ class BSTNode:
             if self.right:
                 return self.right.contains(target)
             else:
-                return False
+                return False    # Return the maximum value found in the tree
+
             
-    # Return the maximum value found in the tree
     def get_max(self):
         vals = [self.value]
         if self.left:
@@ -88,7 +55,9 @@ class BSTNode:
             vals.append(self.right.get_max())
         return max(vals)
     
+      
     # Call the function `fn` on the value of each node
+    # [Jud] - I suppose we should return the results
     def for_each(self, fn):
         return_node = BSTNode(fn(self.value))
         if self.left: 
@@ -96,44 +65,62 @@ class BSTNode:
         if self.right: 
             return_node.right = self.right.for_each(fn)
         return return_node
-                    
-# foo = BSTNode(10)
-# foo.insert(1)
-
-# foo.insert(20)
-
-# foo.insert(15)
-
-# def plus1(x):
-#     return x + 1
-
-# bar = foo.for_each(plus1)
-# bar
-
+    
     # Part 2 -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
-    def in_order_print(self, node):
-        pass
-
+    def in_order_print(self):
+        if self.left:
+            self.left.in_order_print()
+        print(self.value)
+        if self.right:
+            self.right.in_order_print()
+            
+            
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
-    def bft_print(self, node):
-        pass
+    def bft_print(self):
+        q = deque()
 
+        q.append(self)
+        
+        while len(q) > 0:
+            current_node = q.popleft()
+            if current_node.left:
+                q.append(current_node.left)
+            if current_node.right:
+                q.append(current_node.right)
+                
+            print(current_node.value)
+            
+            
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
-    def dft_print(self, node):
-        pass
+    def dft_print(self):
+        stk = []
+#         print(f'stack length: {len(stk)}')
+        stk.append(self)
+#         print(f'stack length: {len(stk)}')
 
-    # Stretch Goals -------------------------
-    # Note: Research may be required
+        
+        while len(stk) > 0:
+            curr = stk.pop()
+#             print(f'stack length: {len(stk)}')
+            if curr.right:
+                stk.append(curr.right)
+#                 print('added right to stack')
+#                 print(f'stack length: {len(stk)}')
+            if curr.left:
+                stk.append(curr.left)
+#                 print('added left to stack')
+#                 print(f'stack length: {len(stk)}')
+                
+            print(curr.value)
 
-    # Print Pre-order recursive DFT
-    def pre_order_dft(self, node):
-        pass
+# foo = BSTNode(10)
+# foo.insert(1)
+# foo.insert(20)
+# foo.insert(15)
 
-    # Print Post-order recursive DFT
-    def post_order_dft(self, node):
-        pass
+# foo.dft_print()
